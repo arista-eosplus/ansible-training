2d169bbefac1eb9b652a192814c3ebd1d9a5db79
MattH <mhartzel@arista.com>
2019-02-08 13:20:44 -0500

    Add more custom filters.

--- filter_plugins/some_custom_filters.py
+++ filter_plugins/some_custom_filters.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # Custom Filters for Ansible
 
 class FilterModule(object):
@@ -5,14 +6,19 @@ class FilterModule(object):
     def filters(self):
         return {
             'valid_eos_version': self.valid_eos_version,
+            'a_simple_filter': self.a_simple_filter,
         }
 
     def valid_eos_version(self, version_string):
         import re
-        version_search = re.search(r'4.\d+.\d+[A-Za-z]+)', version_string)
-        if not version_search:
-            raise EOSVersionFormatError("{} isn't a valid EOS version string.".format(version_string))
+        match = re.match(r'4\.\d+\.\d+[A-Za-z]+)', version_string)
+        if not match:
+            raise EOSVersionFormatError("%s isn't a valid EOS version string." % version_string)
         return True
+
+    def a_simple_filter(self, string):
+        updated_string = string + ' String Modified'
+        return updated_string
 
 class EOSVersionFormatError(Exception):
     pass
